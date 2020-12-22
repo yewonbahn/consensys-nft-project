0f4473eb7aa3f87b80cedbe60907c065b2f2b8a1
chris <daiyitastic@gmail.com>
2020-12-21 18:55:00 -0800

    dingo part selector

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import React, { useEffect, useState } from "react";
 import { Sprite, Stage } from "@inlet/react-pixi";
 import { css } from "@emotion/react/macro";
 import "./App.css";
+
 import dingoLines from "./dingolines.png";
 import dingoBase from "./dingo-base.png";
 import dingoEarLeft from "./dingo-ear-left.png";
 import dingoEarRight from "./dingo-ear-right.png";
 import dingoMaskRight from "./dingo-mask-right.png";
+
+import iconBody from "./icon-body.png";
+import iconEarLeft from "./icon-ear-left.png";
+import iconEarRight from "./icon-ear-right.png";
 
 const W = 600;
 const H = 540;
@@ -20,10 +25,12 @@ const colorToHex = {
 };
 
 const DefaultDingoConfig = {
-  base: null,
-  earLeft: null,
-  earRight: null,
-  maskRight: null,
+  base: {
+    style: 0,
+  },
+  earLeft: {},
+  earRight: {},
+  maskRight: {},
 };
 
 const partToImg = {
@@ -35,22 +42,19 @@ const partToImg = {
 
 type DingoColor = keyof typeof colorToHex | null;
 type DingoPart = keyof typeof partToImg;
+type DingoPartStyle = number;
 
 type DingoConfig = {
-  [part in DingoPart]: DingoColor;
+  [part in DingoPart]: {
+    style?: DingoPartStyle;
+    color?: DingoColor;
+  };
 };
-
-// const config = {
-//   earLeft: "red",
-//   earRight: "",
-//   base: "red",
-// };
 
 function App() {
   const [dingoConfig, setDingoConfig] = useState<DingoConfig>(
     DefaultDingoConfig
   );
-  const [tint, setTint] = useState<number>(0x27e10e);
   const [view, setView] = useState<string>("body");
 
   useEffect(() => {}, [dingoConfig]);
@@ -78,146 +82,12 @@ function App() {
         <Sprite width={W} height={H} image={dingoLines} />
       </Stage>
 
-      <Configorator view={view} setView={setView} />
-
-      <form>
-        <h3>body color</h3>
-        <li>
-          <input
-            type="radio"
-            name="base"
-            value="none"
-            id="basenone"
-            defaultChecked
-            onChange={(e) =>
-              setDingoConfig((prevState) => {
-                const state: DingoConfig = { ...prevState };
-                state.base = null;
-                return state;
-              })
-            }
-          ></input>
-          <label htmlFor="basenone">none</label>
-        </li>
-        <li>
-          <input
-            type="radio"
-            name="base"
-            value="red"
-            id="basered"
-            onChange={(e) =>
-              setDingoConfig((prevState) => {
-                const state: DingoConfig = { ...prevState };
-                state.base = "red";
-                return state;
-              })
-            }
-          ></input>
-          <label htmlFor="basered">red</label>
-        </li>
-        <li>
-          <input
-            type="radio"
-            name="base"
-            value="cream"
-            id="basecream"
-            onChange={(e) =>
-              setDingoConfig((prevState) => {
-                const state: DingoConfig = { ...prevState };
-                state.base = "cream";
-                return state;
-              })
-            }
-          ></input>
-          <label htmlFor="basecream">cream</label>
-        </li>
-        <li>
-          <input
-            type="radio"
-            name="base"
-            value="blue"
-            id="baseblue"
-            onChange={(e) =>
-              setDingoConfig((prevState) => {
-                const state: DingoConfig = { ...prevState };
-                state.base = "blue";
-                return state;
-              })
-            }
-          ></input>
-          <label htmlFor="baseblue">blue</label>
-        </li>
-        <li>
-          <input
-            type="radio"
-            name="base"
-            value="gray"
-            id="basegray"
-            onChange={(e) =>
-              setDingoConfig((prevState) => {
-                const state: DingoConfig = { ...prevState };
-                state.base = "gray";
-                return state;
-              })
-            }
-          ></input>
-          <label htmlFor="basegray">gray</label>
-        </li>
-        <h3>dingo ears</h3>
-        <Checkbox
-          dingoConfig={dingoConfig}
-          setDingoConfig={setDingoConfig}
-          color="red"
-          part="earLeft"
-        >
-          left ear
-        </Checkbox>
-        <Checkbox
-          dingoConfig={dingoConfig}
-          setDingoConfig={setDingoConfig}
-          color="red"
-          part="earRight"
-        >
-          right ear
-        </Checkbox>
-        <Checkbox
-          dingoConfig={dingoConfig}
-          setDingoConfig={setDingoConfig}
-          color="red"
-          part="maskRight"
-        >
-          right mask
-        </Checkbox>
-        <Checkbox
-          dingoConfig={dingoConfig}
-          setDingoConfig={setDingoConfig}
-          color="blue"
-          part="earLeft"
-        >
-          left ear
-        </Checkbox>
-        <Checkbox
-          dingoConfig={dingoConfig}
-          setDingoConfig={setDingoConfig}
-          color="blue"
-          part="earRight"
-        >
-          right ear
-        </Checkbox>
-        <Checkbox
-          dingoConfig={dingoConfig}
-          setDingoConfig={setDingoConfig}
-          color="blue"
-          part="maskRight"
-        >
-          right mask
-        </Checkbox>
-        <h3>dingo tint</h3>
-        <input
-          value={tint.toString(16)}
-          onChange={(e) => setTint(parseInt(e.target.value, 16))}
-        />
-      </form>
+      <Configorator
+        dingoConfig={dingoConfig}
+        view={view}
+        setDingoConfig={setDingoConfig}
+        setView={setView}
+      />
     </div>
   );
 }
@@ -230,14 +100,21 @@ const DingoDrawing = ({ config }: DingoDrawingProps) => {
   return (
     <React.Fragment>
       {Object.keys(config).map((part) => {
-        const dingoColor = config[part as DingoPart];
-        return dingoColor === null ? null : (
+        let { color, style } = config[part as DingoPart];
+        if (!style) {
+          return null;
+        }
+        if (!color) {
+          color = "blue"; // TODO default while trying to set colours
+        }
+        return (
           <Sprite
-            key={`${part}-${dingoColor}`}
+            key={`${part}-${color}-${style}`}
             width={W}
             height={H}
             image={partToImg[part as DingoPart]}
-            tint={colorToHex[dingoColor]}
+            // tint={colorToHex[color]}
+            tint={0x27e10e} // TODO
           />
         );
       })}
@@ -245,101 +122,289 @@ const DingoDrawing = ({ config }: DingoDrawingProps) => {
   );
 };
 
-const Checkbox = ({
-  dingoConfig,
-  part,
-  color,
-  setDingoConfig,
-  children,
-}: {
-  dingoConfig: DingoConfig;
-  part: DingoPart;
-  color: DingoColor;
-  setDingoConfig: React.Dispatch<React.SetStateAction<DingoConfig>>;
-  children: string;
-}) => {
-  return (
-    <div>
-      <label>
-        <input
-          type="checkbox"
-          value={color ? color : "none"}
-          checked={dingoConfig[part] === color}
-          onChange={(e) =>
-            setDingoConfig((prevState) => {
-              const state: DingoConfig = { ...prevState };
-              state[part] = e.target.checked ? color : null;
-              return state;
-            })
-          }
-        />
-        [{color}] {children}
-      </label>
-    </div>
-  );
+const color = {
+  lightblue: "aliceblue",
+  gray: "#ccc",
+  yellow: "#fff9cd",
+  red: "#ff0000",
 };
 
 const Configorator = ({
+  dingoConfig,
   setView,
+  setDingoConfig,
   view,
 }: {
+  dingoConfig: DingoConfig;
   setView: React.Dispatch<React.SetStateAction<string>>;
+  setDingoConfig: React.Dispatch<React.SetStateAction<DingoConfig>>;
   view: string;
 }) => {
   return (
     <div
       css={css`
+        margin-top: 20px;
         display: flex;
         flex-direction: row;
       `}
     >
-      <div>
-        <ViewTitle name="body" setView={setView}>
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          align-items: stretch;
+          padding: 5px 0 10px 10px;
+        `}
+      >
+        <ViewTitle name="body" setView={setView} currentView={view}>
           Body
         </ViewTitle>
-        <ViewTitle name="earL" setView={setView}>
+        <ViewTitle name="earL" setView={setView} currentView={view}>
           Ear L
         </ViewTitle>
-        <ViewTitle name="earR" setView={setView}>
+        <ViewTitle name="earR" setView={setView} currentView={view}>
           Ear R
         </ViewTitle>
-        <ViewTitle name="maskL" setView={setView}>
+        <ViewTitle name="maskL" setView={setView} currentView={view}>
           Mask L
         </ViewTitle>
-        <ViewTitle name="maskR" setView={setView}>
+        <ViewTitle name="maskR" setView={setView} currentView={view}>
           Mask R
         </ViewTitle>
-        <ViewTitle name="mottle" setView={setView}>
-          Mottle
+        <ViewTitle name="speckles" setView={setView} currentView={view}>
+          Speckles
         </ViewTitle>
       </div>
-      <ConfigView view={view} />
+      <ConfigView
+        dingoConfig={dingoConfig}
+        view={view}
+        setDingoConfig={setDingoConfig}
+      />
     </div>
   );
 };
 
-const ConfigView = ({ view }: { view: string }) => {
-  return <p>{view}</p>;
+const Option = ({
+  img,
+  dingoPart,
+  dingoPartStyle,
+  dingoConfig,
+  setDingoConfig,
+}: {
+  img?: string;
+  dingoPart: DingoPart;
+  dingoPartStyle: DingoPartStyle;
+  dingoConfig: DingoConfig;
+  setDingoConfig: React.Dispatch<React.SetStateAction<DingoConfig>>;
+}) => {
+  return (
+    <label
+      css={css`
+        margin: 5px;
+      `}
+    >
+      <input
+        css={css`
+          /* HIDE RADIO */
+          &[type="radio"] {
+            position: absolute;
+            opacity: 0;
+            width: 0;
+            height: 0;
+          }
+
+          /* IMAGE STYLES */
+          &[type="radio"] + img,
+          &[type="radio"] + div {
+            cursor: pointer;
+            filter: brightness(60%);
+            outline: 2px solid ${color.gray};
+          }
+
+          /* CHECKED STYLES */
+          &[type="radio"]:checked + img,
+          &[type="radio"]:checked + div {
+            outline: 2px solid ${color.red};
+          }
+        `}
+        key={`${dingoPart}-${dingoPartStyle}`}
+        type="radio"
+        name={dingoPart}
+        value={dingoPartStyle}
+        defaultChecked={dingoConfig[dingoPart].style === dingoPartStyle}
+        onChange={(e) => {
+          setDingoConfig((prevState) => {
+            const state: DingoConfig = { ...prevState };
+            state[dingoPart].style = dingoPartStyle;
+            return state;
+          });
+        }}
+      />
+      {img ? (
+        <img width={100} height={100} alt={dingoPart} src={img} />
+      ) : (
+        <div
+          css={css`
+            width: 100px;
+            height: 100px;
+            color: ${color.gray};
+          `}
+        >
+          none
+        </div>
+      )}
+    </label>
+  );
+};
+
+// CSS filter generator to convert from black to target hex color
+// https://codepen.io/sosuke/pen/Pjoqqp
+const ConfigView = ({
+  dingoConfig,
+  view,
+  setDingoConfig,
+}: {
+  dingoConfig: DingoConfig;
+  view: string;
+  setDingoConfig: React.Dispatch<React.SetStateAction<DingoConfig>>;
+}) => {
+  let View;
+  switch (view) {
+    case "body":
+      View = (
+        <React.Fragment>
+          <Option
+            dingoPart="base"
+            dingoPartStyle={0}
+            dingoConfig={dingoConfig}
+            img={iconBody}
+            setDingoConfig={setDingoConfig}
+          />
+        </React.Fragment>
+      );
+      break;
+    case "earL":
+      View = (
+        <React.Fragment>
+          <Option
+            dingoConfig={dingoConfig}
+            dingoPart="earLeft"
+            dingoPartStyle={0}
+            setDingoConfig={setDingoConfig}
+          />
+          <Option
+            dingoConfig={dingoConfig}
+            dingoPart="earLeft"
+            dingoPartStyle={1}
+            img={iconEarLeft}
+            setDingoConfig={setDingoConfig}
+          />
+        </React.Fragment>
+      );
+      break;
+    case "earR":
+      View = (
+        <React.Fragment>
+          <Option
+            dingoConfig={dingoConfig}
+            dingoPart="earRight"
+            dingoPartStyle={0}
+            setDingoConfig={setDingoConfig}
+          />
+          <Option
+            dingoConfig={dingoConfig}
+            dingoPart="earRight"
+            dingoPartStyle={1}
+            img={iconEarRight}
+            setDingoConfig={setDingoConfig}
+          />
+        </React.Fragment>
+      );
+      break;
+    default:
+      View = <div>coming soon</div>;
+  }
+  return (
+    <div
+      css={css`
+        border-left: 3px solid ${color.gray};
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        padding: 10px;
+        width: 440px;
+        flex: 1 1 auto;
+      `}
+    >
+      {View}
+    </div>
+  );
 };
 
 const ViewTitle = ({
   children,
+  currentView,
   name,
   setView,
 }: {
   children: string;
+  currentView: string;
   name: string;
   setView: React.Dispatch<React.SetStateAction<string>>;
 }) => {
+  const isActive = currentView === name;
   return (
-    <button
+    <ResetButton
+      css={css`
+        display: block;
+        border-bottom: 2px solid #c6d9ea;
+        text-align: left;
+        padding: 10px 20px 0 5px;
+        background: ${isActive ? color.yellow : "transparent"};
+
+        &:hover {
+          background: ${color.lightblue};
+        }
+      `}
       onClick={() => {
         setView(name);
       }}
     >
       {children}
-    </button>
+    </ResetButton>
   );
 };
+
+const ResetButton = (props: any) => (
+  <button
+    css={css`
+      /* RESET START */
+      border: none;
+      margin: 0;
+      padding: 0;
+      width: auto;
+      overflow: visible;
+
+      background: transparent;
+
+      /* inherit font & color from ancestor */
+      color: inherit;
+      font: inherit;
+
+      /* Normalize "line-height". Cannot be changed from "normal" in Firefox 4+. */
+      line-height: normal;
+
+      /* Corrects font smoothing for webkit */
+      -webkit-font-smoothing: inherit;
+      -moz-osx-font-smoothing: inherit;
+
+      /* Corrects inability to style clickable "input" types in iOS */
+      -webkit-appearance: none;
+
+      /* RESET END */
+    `}
+    {...props}
+  />
+);
 
 export default App;
